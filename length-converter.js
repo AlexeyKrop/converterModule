604e1e3a3f926184a5989efabfc284e862a43b7c
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-02-04 15:38:11 +0300

    fix scripts

--- length-converter.js
+++ length-converter.js
@@ -19,37 +19,37 @@ form.addEventListener("click", (e) => {
       inch.value = km.value * 39370.1;
       mile.value = km.value / 1.6;
       yard.value = km.value * 1093.613888889;
-    } else if (t.value !== "") {
-      kg.value = t.value * 1000;
-      gr.value = t.value * 1000 * 1000;
-      lb.value = t.value * 2204.6;
-      oz.value = t.value * 35.3 * 1000;
-      kn.value = t.value * 9.8 * 1000;
-    } else if (gr.value !== "") {
-      kg.value = gr.value / 1000;
-      t.value = gr.value / 1000 / 1000;
-      lb.value = gr.value / 453.6;
-      oz.value = gr.value / 28.35;
-      kn.value = gr.value * 0.00981;
-    } else if (lb.value !== "") {
-      kg.value = lb.value / 2.2;
-      t.value = lb.value / 2204.6;
-      gr.value = lb.value * 453.6;
-      oz.value = lb.value * 16;
-      kn.value = lb.value * 4.44822;
-    } else if (oz.value !== "") {
-      kg.value = oz.value / 35.274;
-      t.value = oz.value * 28.34 * 10e-7;
-      gr.value = oz.value * 28.34;
-      lb.value = oz.value / 16;
-      kn.value = oz.value / 3.57142;
-    } else if (kn.value !== "") {
-      kg.value = kn.value / 9.8;
-      t.value = kn.value / 9.8 / 1000;
-      gr.value = kn.value / 0.00981;
-      lb.value = kn.value / 4.45;
-      oz.value = kn.value * 3.59694;
-    }
+    // } else if (t.value !== "") {
+    //   kg.value = t.value * 1000;
+    //   gr.value = t.value * 1000 * 1000;
+    //   lb.value = t.value * 2204.6;
+    //   oz.value = t.value * 35.3 * 1000;
+    //   kn.value = t.value * 9.8 * 1000;
+    // } else if (gr.value !== "") {
+    //   kg.value = gr.value / 1000;
+    //   t.value = gr.value / 1000 / 1000;
+    //   lb.value = gr.value / 453.6;
+    //   oz.value = gr.value / 28.35;
+    //   kn.value = gr.value * 0.00981;
+    // } else if (lb.value !== "") {
+    //   kg.value = lb.value / 2.2;
+    //   t.value = lb.value / 2204.6;
+    //   gr.value = lb.value * 453.6;
+    //   oz.value = lb.value * 16;
+    //   kn.value = lb.value * 4.44822;
+    // } else if (oz.value !== "") {
+    //   kg.value = oz.value / 35.274;
+    //   t.value = oz.value * 28.34 * 10e-7;
+    //   gr.value = oz.value * 28.34;
+    //   lb.value = oz.value / 16;
+    //   kn.value = oz.value / 3.57142;
+    // } else if (kn.value !== "") {
+    //   kg.value = kn.value / 9.8;
+    //   t.value = kn.value / 9.8 / 1000;
+    //   gr.value = kn.value / 0.00981;
+    //   lb.value = kn.value / 4.45;
+    //   oz.value = kn.value * 3.59694;
+    // }
   }
 });
 const validation = () => {
